d443343865f03cc39c2b589ac3f6f308f2ca2992
Amitmagadum1545 <amitmagadum1008@gmail.com>
2022-02-24 19:28:34 +0530

    [Amit] Printing Arrays Of Different Data Type

---
+++ PrintArrayGenerics/PrintArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintArrayGenerics
+{
+    internal class PrintArray
+    {
+        public static void toPrint(int[] intArray)
+        {
+            foreach (int item in intArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static void toPrint(double[] doubleArray)
+        {
+            foreach (double item in doubleArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static void toPrint(char[] charArray)
+        {
+            foreach (char item in charArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ PrintArrayGenerics/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintArrayGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5, 6, 7 };
+            double[] doubleArray = { 1.2, 4.5, 6.5, 8.5, 89.2345688 };
+            char[] charArray = { 'A', 'B', 'C', 'D' };
+
+            PrintArray.toPrint(intArray);
+            PrintArray.toPrint(doubleArray);
+            PrintArray.toPrint(charArray);
+        }
+    }
+}
